1e4f193aa83a9ff3e1126bf2d8b578aad2cbfda3
Replit user <>
2023-10-29 09:47:42 +0000

    Refactor: Enhance logging and restructure index.js

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const app = express();
+const bodyParser = require('body-parser');
+const urlRoutes = require('./controllers/urlController'); 
 
-// Basic Configuration
-const port = process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-
 app.use('/public', express.static(`${process.cwd()}/public`));
-
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+    if (process.env.NODE_ENV !== 'development') return next();
+    const currentTime = new Date().toISOString();
+    console.log(`[${currentTime}] ${req.method} ${req.originalUrl}`);
+    console.log(`Client IP: ${req.ip}`);
+    console.log(`User Agent: ${req.headers['user-agent']}`);
+    console.log('Headers:', JSON.stringify(req.headers, null, 2));
+    console.log('Body:', JSON.stringify(req.body, null, 2));
+    console.log('Query:', JSON.stringify(req.query, null, 2));
+    console.log('--- End of Request Info ---\n');
+    next();
 });
 
-/** 
-You can POST a URL to /api/shorturl and get a JSON response with original_url and short_url properties. Here's an example: { original_url : 'https://freeCodeCamp.org', short_url : 1}
-When you visit /api/shorturl/<short_url>, you will be redirected to the original URL.
-If you pass an invalid URL that doesn't follow the valid http://www.example.com format, the JSON response will contain { error: 'invalid url' }
-*/
-app.post('/api/shorturl', function(){
-  throw new Error('Not implemented');
-});
+app.use(urlRoutes);
 
-
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
-});
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
