1905f00df3be823a14086f489365ee7f8a0fd5b4
Replit user <>
2023-10-29 08:07:11 +0000

    feat(api): Add /api/shorturl endpoint with placeholder implementation

--- index.js
+++ index.js
@@ -14,10 +14,15 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+/** 
+You can POST a URL to /api/shorturl and get a JSON response with original_url and short_url properties. Here's an example: { original_url : 'https://freeCodeCamp.org', short_url : 1}
+When you visit /api/shorturl/<short_url>, you will be redirected to the original URL.
+If you pass an invalid URL that doesn't follow the valid http://www.example.com format, the JSON response will contain { error: 'invalid url' }
+*/
+app.post('/api/shorturl', function(){
+  throw new Error('Not implemented');
 });
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
